086b3948ffc39e897c4f1f1fe25c6cd1a9a1d66d
said1854 <msaliogullari@gmail.com>
2023-03-22 22:48:09 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header({money}) {
+  return (
+      <div className='bg-slate-400'>{money}$ paraniz var</div>
+  )
+}
+
+export default Header
---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Product({product}) {
+  return (
+      <h3>
+        {product.title}  
+    </h3>
+  )
+}
+
+export default Product
